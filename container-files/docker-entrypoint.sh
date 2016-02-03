dc540df87a08fdeaf60ed9976c6559422341daba
HaukeErnst <hauke.ernst@astrium.eads.net>
2016-02-03 15:37:42 +0100

    Bugfixing

--- container-files/docker-entrypoint.sh
+++ container-files/docker-entrypoint.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-cd /templates
-echo "Configuring LocalSettings.php"
+cd /var/www/html/maintenance
+echo "Configuring MediaWiki with install.php"
 echo $MEDIAWIKI_SITE_NAME
 echo $MEDIAWIKI_DB_HOST
 echo $MEDIAWIKI_DB_USER
 echo $MEDIAWIKI_DB_PASSWORD
 echo $MEDIAWIKI_DB_NAME
+echo $MEDIAWIKI_ADMIN_PASSWORD
+echo $MEDIAWIKI_ADMIN_USER
 
-sed -i "s/%%SITENAME%%/$MEDIAWIKI_SITE_NAME/g" LocalSettings.php
-sed -i "s/%%DB_HOSTNAME%%/$MEDIAWIKI_DB_HOST/g" LocalSettings.php
-sed -i "s/%%DB_USER%%/$MEDIAWIKI_DB_USER/g" LocalSettings.php
-sed -i "s/%%DB_PASSWORD%%/$MEDIAWIKI_DB_PASSWORD/g" LocalSettings.php
-sed -i "s/%%DB_NAME%%/$MEDIAWIKI_DB_NAME/g" LocalSettings.php
 
-mv LocalSettings.php /var/www/html/
+php install.php --scriptpath="" --dbname=$MEDIAWIKI_DB_NAME --dbuser=$MEDIAWIKI_DB_USER --dbpass=$MEDIAWIKI_DB_PASSWORD --dbserver=$MEDIAWIKI_DB_HOST --installdbpass=$MEDIAWIKI_DB_PASSWORD --installdbuser=$MEDIAWIKI_DB_USER --pass=$MEDIAWIKI_ADMIN_PASSWORD $MEDIAWIKI_SITE_NAME $MEDIAWIKI_ADMIN_USER
 
 httpd -e debug -DFOREGROUND
